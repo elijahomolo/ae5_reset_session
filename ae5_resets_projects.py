9d1cd3f25236a9dab7348512065f06ee381151b9
elijahomolo <eomolo11@gmail.com>
2019-08-05 13:22:41 -0500

    first commit

---
+++ ae5_resets_projects.py
@@ -0,0 +1,36 @@
+import os
+
+#get active sessions
+def active_deployments():
+    active_sessions="kubectl get deploy | grep session | cut -d' ' -f1"
+    session=os.popen(active_sessions).read()
+    sessions_list = session.split("\n")
+    new_list = []
+    for each in sessions_list:
+        s_id=('a1-'+each.strip('anaconda-session-'))
+        new_list.append(s_id)
+
+    #new_list = filter(None, new_list)
+    return new_list
+
+#get DB sessions
+def db_sessions():
+    db_sessions="ae5 session list --columns=id --no-header"
+    session=os.popen(db_sessions).read()
+    db_sessions_list = session.split("\n")
+    #db_sessions_list = filter(None, db_sessions_list)
+
+    return db_sessions_list
+
+#find db sessions with no matching deployment and delete it
+def delete_session(new_list, db_sessions_list):
+    delete="ae5 session delete"
+    for session in db_sessions_list:
+        if session not in new_list:
+            os.system(stop + ' ' + session + ' ' + '--yes')
+        else:
+            pass
+
+new_list=active_deployments()
+db_sessions_list=db_sessions()
+delete_session(new_list, db_sessions_list)
